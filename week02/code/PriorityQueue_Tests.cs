865f5f7d2899a021524d1a619a42afe47023d921
Adafa Ralph <yme7ir@gmail.com>
2026-03-10 19:06:59 -0400

    week2

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,49 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario:
+    // Expected Result:
+    // Defect(s) Found:
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Item1", 2);
+        priorityQueue.Enqueue("Item2", 3);
+        priorityQueue.Enqueue("Item3", 5);
+
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("Item3", result);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario:
+    // Expected Result:
+    // Defect(s) Found:
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("First", 10);
+        priorityQueue.Enqueue("Second", 10);
+        priorityQueue.Enqueue("Third", 5);
+
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("First", result);
     }
 
     // Add more test cases as needed below.
+    [TestMethod]
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Assert.AreEqual("The queue is empty.", ex.Message);
+        }
+    }
 }
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,10 +1,10 @@
 /// <summary>
-/// This queue is circular.  When people are added via AddPerson, then they are added to the 
+/// This queue is circular.  When people are added via AddPerson, then they are added to the
 /// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
 /// in the queue is saved to be returned and then they are placed back into the back of the queue.  Thus,
-/// each person stays in the queue and is given turns.  When a person is added to the queue, 
+/// each person stays in the queue and is given turns.  When a person is added to the queue,
 /// a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-/// less than they will stay in the queue forever.  If a person is out of turns then they will 
+/// less than they will stay in the queue forever.  If a person is out of turns then they will
 /// not be added back into the queue.
 /// </summary>
 public class TakingTurnsQueue
@@ -13,24 +13,12 @@ public class TakingTurnsQueue
 
     public int Length => _people.Length;
 
-    /// <summary>
-    /// Add new people to the queue with a name and number of turns
-    /// </summary>
-    /// <param name="name">Name of the person</param>
-    /// <param name="turns">Number of turns remaining</param>
     public void AddPerson(string name, int turns)
     {
         var person = new Person(name, turns);
         _people.Enqueue(person);
     }
 
-    /// <summary>
-    /// Get the next person in the queue and return them. The person should
-    /// go to the back of the queue again unless the turns variable shows that they 
-    /// have no more turns left.  Note that a turns value of 0 or less means the 
-    /// person has an infinite number of turns.  An error exception is thrown 
-    /// if the queue is empty.
-    /// </summary>
     public Person GetNextPerson()
     {
         if (_people.IsEmpty())
@@ -40,8 +28,16 @@ public class TakingTurnsQueue
         else
         {
             Person person = _people.Dequeue();
-            if (person.Turns > 1)
+
+            // Fix: Added logic to handle infinite turns (0 or less)
+            if (person.Turns <= 0)
             {
+                // Infinite turns: put them back exactly as they are
+                _people.Enqueue(person);
+            }
+            else if (person.Turns > 1)
+            {
+                // Finite turns: reduce count and put back if they still have turns left
                 person.Turns -= 1;
                 _people.Enqueue(person);
             }
